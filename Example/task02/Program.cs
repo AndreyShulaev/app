0f11e6eb45fed94637496ce9ab3a61a18ac97ab9
Andrey Shulaev <Shul_aa>
2022-10-20 16:19:33 +0300

    Версия 1

---
+++ Example/task02/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Введите два числа: ");
+int a = Convert.ToInt32(Console.ReadLine ());
+int b = Convert.ToInt32(Console.ReadLine ());
+if (b > a)
+{
+    Console.Write("Максимальное число: "+b);
+}
+else
+{
+    Console.Write("Максимальное число: "+a);
+}         
