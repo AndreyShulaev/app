f148f2d2f709f26b9a0c3624e9c26f893104c99f
Andrey Shulaev <Shul_aa>
2022-10-24 18:10:42 +0300

    Task13_версия1

---
+++ Example/task13/Program.cs
@@ -0,0 +1,12 @@
+﻿static string func(string x) { return x.Substring(2, 1); }
+Console.WriteLine ("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine ());
+string text = a.ToString();
+if (a < 100) 
+    {
+        Console.WriteLine ("третьей цифры нет!");
+    }    
+    else
+    {        
+        Console.Write("Третья цифра: "+ func(text));
+    } 
